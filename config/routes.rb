e517589d6b87fc53893a4e194ab5ff7c546fabb9
elboraki <younes.elboraki@gmail.com>
2020-05-13 23:36:43 +0000

    add student resources and sign up functionality for student

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'courses#index'
   get '/about', to: 'pages#about'
-
+  resources :students, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
